efbe9150f5c184b9b74efd166b9b2ec10f11388d
cchabanois <cchabanois@gmail.com>
2013-01-25 16:33:30 +0100

    more comments for TestRunIds

--- testInProgress-plugin/src/main/java/org/jenkinsci/plugins/testinprogress/events/build/TestRunIds.java
+++ testInProgress-plugin/src/main/java/org/jenkinsci/plugins/testinprogress/events/build/TestRunIds.java
@@ -6,27 +6,34 @@ import java.util.List;
 import org.jenkinsci.plugins.testinprogress.ITestRunIds;
 
 /**
- * Keeps all run ids for a build.
+ * Generate and Keep all run ids for a build.
  * 
  * @author Cedric Chabanois (cchabanois at gmail.com)
- *
+ * 
  */
 public class TestRunIds implements ITestRunIds {
 	private final List<String> runIds = new ArrayList<String>();
-	
+
 	public synchronized List<String> getRunIds() {
 		return new ArrayList<String>(runIds);
 	}
 
+	/**
+	 * Create a run id and add it to the sets of run ids for the build.
+	 * 
+	 * @param proposedRunId
+	 *            the proposed run id (generally the main suite name)
+	 * @return an unique run id for the build
+	 */
 	public synchronized String addRunId(String proposedRunId) {
 		String runId = proposedRunId;
 		int num = 0;
 		while (runIds.contains(proposedRunId)) {
 			num++;
-			runId = proposedRunId +"-"+Integer.toString(num);
+			runId = proposedRunId + "-" + Integer.toString(num);
 		}
 		runIds.add(runId);
 		return runId;
 	}
-	
+
 }
